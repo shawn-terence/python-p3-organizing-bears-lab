03d43731153f69209f6ed103be86cd4948ca358b
terence <terenceshawn18@outlook.com>
2023-12-18 17:03:36 +0300

    done

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears( name,age,sex,color,temperament,alive) 
+VALUES('Mr. Chocolate',10,'M','brown','safe',1),
+    ('Rowdy',10,'M','brown','safe',1),
+    ('Tabitha',10,'F','brown','safe',1),
+    ('Sergeant Brown',10,'M','brown','safe',1),
+    ('Melissa',10,'F','brown','safe',1),
+    ('Grinch',10,'M','brown','safe',1),
+    ('Wendy',10,'F','brown','safe',1),
+    (NULL,10,'M','brown','safe',1);
